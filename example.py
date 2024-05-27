f4f354486c75e8eb43060e89446e9ff8a56eb520
RobinGeens <robin.geens@kuleuven.be>
2024-05-27 12:57:25 +0200

    incorporate temporary zigzag subfolder

--- example.py
+++ example.py
@@ -6,12 +6,11 @@ from zigzag.visualization.results.plot_cme import (
 from zigzag.visualization.results.print_mapping import print_mapping
 from zigzag.visualization.graph.memory_hierarchy import visualize_memory_hierarchy_graph
 
-
-model = "resnet"
-workload_path = "inputs/workload/resnet18.yaml"
+model = "custom_transformer"
+workload_path = "out/custom_transformer.onnx"
 accelerator_path = "inputs/hardware/tpu_like.yaml"
 mapping_path = "inputs/mapping/tpu_like.yaml"
-pickle_filename = f"outputs/TPU-{model}-saved_list_of_cmes.pickle"
+pickle_filename = f"out/TPU-{model}-saved_list_of_cmes.pickle"
 
 
 energy, latency, cmes = api.get_hardware_performance_zigzag(
@@ -28,12 +27,11 @@ with open(pickle_filename, "rb") as fp:
     cmes = pickle.load(fp)
 
 
-bar_plot_cost_model_evaluations_breakdown(cmes, save_path="outputs/plot_breakdown.png")
-
+bar_plot_cost_model_evaluations_breakdown(cmes, save_path="out/plot_breakdown.png")
 
 visualize_memory_hierarchy_graph(
     cmes[0].accelerator.cores[0].memory_hierarchy,
-    save_path="outputs/mem_hierarchy.png",
+    save_path="out/mem_hierarchy.png",
 )
 
 for cme in cmes:
