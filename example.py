bc871784718690c2fd2260724fec301b50db6122
RobinGeens <robin.geens@kuleuven.be>
2024-05-28 15:44:26 +0200

    cleanup

--- example.py
+++ example.py
@@ -9,25 +9,27 @@ from zigzag.visualization.graph.memory_hierarchy import visualize_memory_hierarc
 model = "custom_transformer"
 workload_path = "out/custom_transformer.onnx"
 accelerator_path = "inputs/hardware/tpu_like.yaml"
-mapping_path = "inputs/mapping/tpu_like.yaml"
+mapping_path = "inputs/mapping/default.yaml"
 pickle_filename = f"out/TPU-{model}-saved_list_of_cmes.pickle"
+RE_RUN = True
 
-
-energy, latency, cmes = api.get_hardware_performance_zigzag(
-    workload=workload_path,
-    accelerator=accelerator_path,
-    mapping=mapping_path,
-    opt="energy",
-    pickle_filename=pickle_filename,
-)
-print(f"Total network energy = {energy:.2e} pJ")
-print(f"Total network latency = {latency:.2e} cycles")
+if RE_RUN:
+    energy, latency, cmes = api.get_hardware_performance_zigzag(
+        workload=workload_path,
+        accelerator=accelerator_path,
+        mapping=mapping_path,
+        opt="energy",
+        pickle_filename=pickle_filename,
+        nb_spatial_mappings_generated=1,
+    )
+    print(f"Total network energy = {energy:.2e} pJ")
+    print(f"Total network latency = {latency:.2e} cycles")
 
 with open(pickle_filename, "rb") as fp:
     cmes = pickle.load(fp)
 
 
-bar_plot_cost_model_evaluations_breakdown(cmes, save_path="out/plot_breakdown.png")
+bar_plot_cost_model_evaluations_breakdown(cmes, save_path="plot_breakdown.png")
 
 visualize_memory_hierarchy_graph(
     cmes[0].accelerator.cores[0].memory_hierarchy,
--- export_onnx.py
+++ export_onnx.py
@@ -1,5 +1,7 @@
 # Taken from https://github.com/suvash/nnze2he/blob/main/makemore/src/gpt.py
 
+import math
+import onnx
 import torch
 from torch import Tensor
 import torch.nn as nn
@@ -10,35 +12,32 @@ import config as cfg
 
 device = "cpu"
 dropout = 0.3
-vocab_size = 1_000_000
+VOCAB_SIZE = 1000
 
 
-# single head of self attention
 class Head(nn.Module):
     """one head of self attention"""
 
     def __init__(self, head_size: int):
         super().__init__()  # type: ignore
-        self.key = nn.Linear(cfg.HIDDEN_DIM, head_size, bias=False)
-        self.query = nn.Linear(cfg.HIDDEN_DIM, head_size, bias=False)
-        self.value = nn.Linear(cfg.HIDDEN_DIM, head_size, bias=False)
+        self.key = nn.Linear(cfg.EMBEDDING_DIM, head_size, bias=False)
+        self.query = nn.Linear(cfg.EMBEDDING_DIM, head_size, bias=False)
+        self.value = nn.Linear(cfg.EMBEDDING_DIM, head_size, bias=False)
         # in Pytorch convention a variable that's not a parameter of the model is called a buffer
         self.register_buffer("tril", torch.tril(torch.ones(cfg.SEQ_LEN, cfg.SEQ_LEN)))
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x: Tensor):
         _, T, _ = x.shape
-        # emit keys and queries for x
-        k: Tensor = self.key(x)  # (B, T, hs)
-        q: Tensor = self.query(x)  # (B, T, hs)
-        # compute attention
-        wei: Tensor = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B, T, T)
-        wei = F.softmax(wei, dim=-1)  # (B, T, T)
-        wei = self.dropout(wei)  # dropout some of the affinities
-        # perform the weighted aggregation of the values
-        v = self.value(x)  # (B, T, hs)
-        out = wei @ v  # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        key: Tensor = self.key(x)  # (B, T, hs)
+        query: Tensor = self.query(x)  # (B, T, hs)
+        attention: Tensor = query @ key.transpose(-2, -1)  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        attention = attention * math.sqrt(key.shape[-1])
+        attention = attention.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # (B, T, T)
+        logits = F.softmax(attention, dim=-1)  # (B, T, T)
+        logits = self.dropout(logits)
+        value = self.value(x)  # (B, T, hs)
+        out = logits @ value  # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
 
 
@@ -47,13 +46,13 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, head_size: int):
         super().__init__()  # type: ignore
         self.heads = nn.ModuleList([Head(head_size) for _ in range(cfg.NUM_HEAD)])
-        self.proj = nn.Linear(cfg.HIDDEN_DIM, cfg.HIDDEN_DIM)
+        self.proj = nn.Linear(cfg.EMBEDDING_DIM, cfg.EMBEDDING_DIM)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x: Tensor):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)  # outcome of the linear layer to project back into the residual pathway
-        out = self.dropout(out)  # final dropout
+        out = self.proj(out)
+        out = self.dropout(out)
         return out
 
 
@@ -63,12 +62,10 @@ class FeedForward(nn.Module):
     def __init__(self):
         super().__init__()  # type: ignore
         self.net = nn.Sequential(
-            nn.Linear(cfg.HIDDEN_DIM, 4 * cfg.HIDDEN_DIM),  # as mentioned in the paper
+            nn.Linear(cfg.EMBEDDING_DIM, cfg.DIM_FF),
             nn.ReLU(),
-            nn.Linear(
-                4 * cfg.HIDDEN_DIM, cfg.HIDDEN_DIM
-            ),  # projection layer : the final projection back into the residual pathway
-            nn.Dropout(dropout),  # dropout before final projection
+            nn.Linear(cfg.DIM_FF, cfg.EMBEDDING_DIM),
+            nn.Dropout(dropout),
         )
 
     def forward(self, x: Tensor):
@@ -80,15 +77,15 @@ class Block(nn.Module):
 
     def __init__(self):
         super().__init__()  # type: ignore
-        head_size = cfg.HIDDEN_DIM // cfg.NUM_HEAD
+        head_size = cfg.EMBEDDING_DIM // cfg.NUM_HEAD
         self.sa = MultiHeadAttention(head_size)
-        self.ffwd = FeedForward()
-        self.ln1 = nn.LayerNorm(cfg.HIDDEN_DIM)
-        self.ln2 = nn.LayerNorm(cfg.HIDDEN_DIM)
+        self.feed_forward = FeedForward()
+        self.ln1 = nn.LayerNorm(cfg.EMBEDDING_DIM)
+        self.ln2 = nn.LayerNorm(cfg.EMBEDDING_DIM)
 
     def forward(self, x: Tensor):
         x = x + self.sa(self.ln1(x))
-        x = x + self.ffwd(self.ln2(x))
+        x = x + self.feed_forward(self.ln2(x))
         return x
 
 
@@ -97,11 +94,11 @@ class LanguageModel(nn.Module):
     def __init__(self):
         super().__init__()  # type: ignore
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, cfg.HIDDEN_DIM)
-        self.position_embedding_table = nn.Embedding(cfg.SEQ_LEN, cfg.HIDDEN_DIM)
+        self.token_embedding_table = nn.Embedding(VOCAB_SIZE, cfg.EMBEDDING_DIM)
+        self.position_embedding_table = nn.Embedding(cfg.SEQ_LEN, cfg.EMBEDDING_DIM)
         self.blocks = nn.Sequential(*[Block() for _ in range(cfg.NUM_LAYER)])
-        self.ln_f = nn.LayerNorm(cfg.HIDDEN_DIM)
-        self.lm_head = nn.Linear(cfg.HIDDEN_DIM, vocab_size)
+        self.layer_norm_final = nn.LayerNorm(cfg.EMBEDDING_DIM)
+        self.de_embed = nn.Linear(cfg.EMBEDDING_DIM, VOCAB_SIZE)
 
     def forward(self, idx: Tensor):
         _, T = idx.shape
@@ -111,36 +108,20 @@ class LanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T, C)
         x = token_emb + pos_emb  # (B, T, C)
         x = self.blocks(x)
-        x = self.ln_f(x)
-        logits = self.lm_head(x)  # (B,T,vocab_size)
+        x = self.layer_norm_final(x)
+        logits = self.de_embed(x)  # (B,T,VOCAB_SIZE)
 
         return logits
 
-    def generate(self, idx: Tensor, max_new_tokens: int):
-        # idx is (B, T) array of indices in the current context
-        for _ in range(max_new_tokens):
-            # crop idx to the last block size token
-            idx_cond = idx[:, -cfg.SEQ_LEN :]
-            # get the predictions
-            logits, _ = self(idx_cond)
-            # focus only on the last time step
-            logits = logits[:, -1, :]  # becomes (B, C)
-            # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1)  # (B, C)
-            # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
-            # append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
-        return idx
-
 
 if __name__ == "__main__":
+    path = "out/custom_transformer.onnx"
     model = LanguageModel()
     dummy_input = torch.randint(low=0, high=255, size=(cfg.BATCH_SIZE, cfg.SEQ_LEN))
     torch.onnx.export(  # type: ignore
         model,
         dummy_input,
-        "out/custom_transformer.onnx",
+        path,
         export_params=True,
         opset_version=16,
         do_constant_folding=True,
@@ -148,3 +129,5 @@ if __name__ == "__main__":
         output_names=["output"],
         verbose=False,
     )
+
+    onnx.shape_inference.infer_shapes_path(path, path)  # type: ignore
